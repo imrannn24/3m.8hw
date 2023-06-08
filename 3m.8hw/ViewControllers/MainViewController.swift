9c0922c30a1205782ed8dc20f810afe38f5096b0
imrannn24 <imran200424@icloud.com>
2023-06-08 16:25:33 +0600

    dz

--- 3m.8hw/ViewControllers/MainViewController.swift
+++ 3m.8hw/ViewControllers/MainViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добро пожаловать!!!"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 24)
+        label.numberOfLines = 8
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
 
-        // Do any additional setup after loading the view.
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().offset(20)
+        }
     }
     
 
